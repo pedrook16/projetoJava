9e7722f5294ff836f3df5d5c49de42fa62f0ab4d
Pedro Henrique <pedrook16@gmail.com>
2019-04-26 23:33:50 -0300

    implementation frontend

---
+++ versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="Aministração do sistema" 
+          sub="Cadastros e cia" />
+    </div>
+</template>
+
+<script>
+import PageTitle from "../template/PageTitle"
+
+export default {
+    name: 'AdminPages',
+    components: {PageTitle}
+}
+</script>
+
+<style>
+
+</style>
